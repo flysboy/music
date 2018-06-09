55944f5f39372286da4838fe38877bbfec8d47cf
cgy <919594060@qq.com>
2018-06-09 12:31:33 +0800

    first

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,4 @@
-// class Test{
-// 	constructor(){
-// 		this.a = 'hello world'
-// 	}
-// }
-
-// let test = new Test()
-
-// document.body.innerHTML = test.a;
-// 
 import 'babel-polyfill';
+import Lottery from './lottery';
 
+const syy=new Lottery();
